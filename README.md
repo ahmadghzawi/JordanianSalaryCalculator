da4a52543f1b881db575895710d39ad257eac205
ahmadghzawi <ahmd.ghzawi@gmail.com>
2020-07-09 00:07:10 +0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-### Jordanian Salary Calculator 
-`Calculate Salaries with or without Income Tax Deduction`
-
-This simple page gives the ability to easily calculate total and net annual and monthly salraies, along with total and monthly income tax and social security deductions when recruiting... 
- 
---
+++ precache-manifest.34d7eed0b21aef08b991ad1ea2446f02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7673e3dd84d38547c9526748112a8e",
+    "url": "/JordanianSalaryCalculator/index.html"
+  },
+  {
+    "revision": "71f53c4517d814a5f0e2",
+    "url": "/JordanianSalaryCalculator/static/css/main.79fe769c.chunk.css"
+  },
+  {
+    "revision": "1e3c298b0c3b6633ae99",
+    "url": "/JordanianSalaryCalculator/static/js/2.3674c11d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JordanianSalaryCalculator/static/js/2.3674c11d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71f53c4517d814a5f0e2",
+    "url": "/JordanianSalaryCalculator/static/js/main.6e3891c8.chunk.js"
+  },
+  {
+    "revision": "564b935bd979ce757f61",
+    "url": "/JordanianSalaryCalculator/static/js/runtime-main.9ae21355.js"
+  }
+]);
