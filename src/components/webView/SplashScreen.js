89e9f6981beb5061383d93d92db0600e30ea98b9
ahmadghzawi <ahmd.ghzawi@gmail.com>
2020-08-04 15:39:07 +0300

    fix splash screen animation error

--- src/components/webView/SplashScreen.js
+++ src/components/webView/SplashScreen.js
@@ -3,10 +3,6 @@ import LandingScreen from "../LandingScreen";
 
 export default function SplashScreen(props) {
 
-  setTimeout(function () {
-    document.getElementById("splash").style.display = "none";
-  }, 2500);
-
   return (
     <div id="splash">
       <LandingScreen {...props} />
